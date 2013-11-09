9aa1d9d25fd6067fb63aa620d41acfb87f978ad9
Mysterion <pericov.ko@gmail.com>
2013-11-10 02:02:12 +0400

    More actions

--- src/main/java/org/mystic/kagan/Controller.java
+++ src/main/java/org/mystic/kagan/Controller.java
@@ -2,15 +2,31 @@ package org.mystic.kagan;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Controller {
+
     @FXML
     Group rootGroup;
+
+    @FXML
+    Button startTestButton;
 
     @FXML
     private void closeWindow() {
         System.out.println(rootGroup.getChildren().size());
         System.exit(0);
+    }
 
+    @FXML
+    private void startKaganTest() {
+        startTestButton.setDisable(true);
+        final String url = Controller.class.getResource("/compare/training-session/1/0.jpg").toString();
+        ImageView sample3 = new ImageView(new Image(url));
+        sample3.setLayoutX(294);
+        sample3.setLayoutY(25);
+        rootGroup.getChildren().add(sample3);
     }
 }
--- src/main/java/org/mystic/kagan/Main.java
+++ src/main/java/org/mystic/kagan/Main.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -17,17 +15,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Group root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        final String url = Main.class.getResource("/compare/training-session/1/0.jpg").toString();
-        ImageView sample3 = new ImageView(new Image(url));
-        sample3.setLayoutX(294);
-        sample3.setLayoutY(25);
-        root.getChildren().add(sample3);
         primaryStage.setTitle("Kagan Test");
         primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
-    }
-
-    public void close() {
-        System.exit(0);
     }
 }
