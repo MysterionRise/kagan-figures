9393d2b9b9959473f63b00d0f5e0849e776fbc57
Mysterion <pericov.ko@gmail.com>
2013-11-10 02:36:02 +0400

    Add instruction

--- src/main/java/org/mystic/kagan/Controller.java
+++ src/main/java/org/mystic/kagan/Controller.java
@@ -3,8 +3,10 @@ package org.mystic.kagan;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
 
 public class Controller {
 
@@ -15,6 +17,9 @@ public class Controller {
     Button startTestButton;
 
     @FXML
+    TextArea instructionText;
+
+    @FXML
     private void closeWindow() {
         System.out.println(rootGroup.getChildren().size());
         System.exit(0);
@@ -23,6 +28,7 @@ public class Controller {
     @FXML
     private void startKaganTest() {
         startTestButton.setDisable(true);
+        instructionText.setVisible(false);
         final String url = Controller.class.getResource("/compare/training-session/1/0.jpg").toString();
         ImageView sample3 = new ImageView(new Image(url));
         sample3.setLayoutX(294);
