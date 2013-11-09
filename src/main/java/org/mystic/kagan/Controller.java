d165ede04d8bc00afa9dbb30d68a897c7174f28d
Mysterion <pericov.ko@gmail.com>
2013-11-10 01:53:25 +0400

    Add event handling for exit button

--- src/main/java/org/mystic/kagan/Controller.java
+++ src/main/java/org/mystic/kagan/Controller.java
@@ -1,4 +1,11 @@
 package org.mystic.kagan;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    private void closeWindow() {
+        System.exit(0);
+    }
 }
--- src/main/java/org/mystic/kagan/Main.java
+++ src/main/java/org/mystic/kagan/Main.java
@@ -2,22 +2,34 @@ package org.mystic.kagan;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        Group root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        final String url = Main.class.getResource("/compare/training-session/1/0.jpg").toString();
+        ImageView sample3 = new ImageView(new Image(url));
+        sample3.setLayoutX(294);
+        sample3.setLayoutY(25);
+        root.getChildren().add(sample3);
+
         primaryStage.setTitle("Kagan Test");
         primaryStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    public void close() {
+        System.exit(0);
     }
 }
